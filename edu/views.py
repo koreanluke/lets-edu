33ff233d5ad1d10558afbac1ca7c9599a07289b3
koreanluke <minjae@letscoding.kr>
2023-09-16 11:31:52 +0900

    edu 수정

--- edu/views.py
+++ edu/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import View
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+class Index(View):
+    template = "index.html"
+    def get(self, request):
+        return render(request, self.template)
