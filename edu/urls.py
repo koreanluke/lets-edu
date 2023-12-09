ef4b23dc9b4195d69b7ca84a1945cb4c9c94e300
koreanluke <minjae@letscoding.kr>
2023-12-09 14:48:55 +0900

    j
    a

--- edu/urls.py
+++ edu/urls.py
@@ -6,5 +6,6 @@ app_name = 'edu'
 urlpatterns = [
     path('',views.Index.as_view(), name='index'),
     path('tag',views.TagStudy.as_view(), name='tag_study'),
-    path('milk',views.palgong.as_view(), name = 'palgong'),
+    path('new', views.NewContent.as_view(), name="new_content"),
+    path('feed/<int:pk>', views.FeedDetail.as_view(), name='feed_detail')
 ]
--- edu/views.py
+++ edu/views.py
@@ -1,6 +1,6 @@
-from django.views.generic import View
+from django.views.generic import View, DetailView
 from django.shortcuts import render, get_object_or_404, redirect
-
+from .models import Feed
 # Create your views here.
 class Index(View):
     template_name = "index.html"
@@ -12,14 +12,32 @@ class TagStudy(View):
     template_name = "tag_study.html"
     
     def get(self, request):
-        return render(request, self.template_name)
+        feeds = Feed.objects.all().order_by('id')
+
+        return render(request, self.template_name,
+                      {'feed_list':feeds})
     
-class palgong(View):
-    template_name = "palgong.html"
+class NewContent(View):
+    template_name = "upload_form.html"
     
     def get(self, request):
+
         return render(request,self.template_name)
     def post(self, request):
-        param = request.POST.get('tea','')
-        print(f"param = {param}")
+        param = request.POST.get('content','')
+        param2= request.FILES.get('up_photo' , False)
+        print(f"param2:{param2}")
+        feed = Feed(content=param, photo=param2)
+        feed.save()
         return redirect("edu:tag_study")
+    
+class FeedDetail(DetailView):
+    model = Feed
+    template_name = "feed/detail.html"
+    
+    def get_context_data(self, **kwargs) :
+        context = super().get_context_data(**kwargs)
+        feed = get_object_or_404(Feed, pk = self.kwargs['pk'])
+        context['feed'] = feed
+        
+        return context
